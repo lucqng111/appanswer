295217107c4fe9c95eb836aaac576d1f67f7562b
Phạm Viết Lực <PhamVietLuc@admins-MacBook-2.local>
2020-04-15 23:06:33 +0700

    confid_db

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,11 +77,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'appanswer',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'localhost',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
